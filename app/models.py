e4dc993c2d4b4ce31994513b0fb635075cce595a
ismailpervez <freelanceismail001@gmail.com>
2021-09-26 18:08:34 +0300

    added a User and Post model classes

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    posts = db.relationship('Post', backref='user', lazy=True)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    '''
+    tags will actually be a list, but since we can't store lists in the database,
+    we will serialize it and store it as a string and when we retrieve, we will
+    deserialize it into a list
+    '''
+    tags = db.Column(db.String(200), nullable=False)
+    upvotes = db.Column(db.Integer, nullable=False)
+    downvotes = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
