d97a1b6a765e4fd9942ec365d2ffd700abd6e645
ismailpervez <freelanceismail001@gmail.com>
2021-09-26 18:02:46 +0300

    added a models.py file for our models

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from app import app
 from flask import render_template
 
+'''
+this is the home route - where the user first lands 
+'''
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/articles')
+def get_articles():
+    return render_template('articles.html')
