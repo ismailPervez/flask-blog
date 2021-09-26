68f0e7adcd05d26bc2f9cae3d556a78cbdced4db
ismailpervez <freelanceismail001@gmail.com>
2021-09-26 18:56:50 +0300

    added a registration form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.forms import RegistrationForm, CreatePost
 
 '''
 this is the home route - where the user first lands 
@@ -20,5 +21,6 @@ if the user wants to sign up - accepts both GET and POST methods
 '''
 @app.route('/register', methods=["GET", "POST"])
 def sign_up():
-    return render_template('register.html')
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
 
