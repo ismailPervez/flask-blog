99185fceed7c007c05e5d327ed9d8dd1e45efcdb
ismailpervez <freelanceismail001@gmail.com>
2021-09-26 19:29:53 +0300

    set up flask mail and added configurations

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import config_options
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 
 # app initialization
 app = Flask(__name__)
@@ -11,6 +12,8 @@ app.config.from_object(config_options['dev_config'])
 db = SQLAlchemy(app)
 # bootstrap initialization
 Bootstrap(app)
+# flask mail initialization
+mail = Mail(app)
 
 # other imports that depend on the variables above
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from app.forms import RegistrationForm, CreatePost
+from app.forms import RegistrationForm, CreatePost, LoginForm
 
 '''
 this is the home route - where the user first lands 
@@ -26,5 +26,6 @@ def sign_up():
 
 @app.route('/login')
 def login():
-    return "login"
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
