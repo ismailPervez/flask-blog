128f0133bccf9b370043f353b2552a6e9ba85835
ismailpervez <freelanceismail001@gmail.com>
2021-09-27 08:22:39 +0300

    added an update route to update the post when liked or disliked

--- app/views.py
+++ app/views.py
@@ -145,3 +145,18 @@ def delete_post(post_id):
         db.session.commit()
         flash('post deleted successfully', 'success')
     return redirect(url_for('get_account'))
+
+# update a post - when liked or disliked
+@app.route('/update/<int:post_id>', methods=['PUT'])
+@login_required
+def update_post(post_id):
+    review = Post.query.filter_by(id=post_id).first()
+    if "upvotes" in request.json:
+        review.upvotes = request.json["upvotes"]
+
+    if "downvotes" in request.json:
+        review.downvotes = request.json["downvotes"]
+
+    db.session.commit()
+
+    return "status: ok"
